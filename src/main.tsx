85a098759753867c6d423ee8a8db648d422bcff9
eashaasif123 <annasahmed1609@gmail.com>
2023-10-15 16:39:28 +0500

    chk

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-     <App/>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
